d23f867b6d88649670abad5b082feb2f175e9f87
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-09-01 22:12:48 -0400

    feature: added settings.py for WHITE_LIST configuration

--- find.py
+++ find.py
@@ -1,17 +1,13 @@
 import os
 import argparse
 from spy_modules.projects import  find_projects
-
-WHITE_LIST = [
-    '/home/dhaval/Hive/projects',
-    '/home/dhaval/.config',
-]
+import settings
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser()
     argParser.add_argument('directory', nargs='?', default=None, help='Module directory')
     args = argParser.parse_args()
-    for dir in WHITE_LIST:
+    for dir in settings.WHITE_LIST:
         projects = find_projects(dir)
         for project in projects:
             if not project.path.startswith(args.directory):
---
+++ settings.py
@@ -0,0 +1,5 @@
+
+WHITE_LIST = [
+    '/home/username/projects',
+    # more directories
+]
